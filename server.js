8ed12375e1ea778192ca55b01c276457bdced484
UnknownEvil <reapor12@hotmail.com>
2016-10-13 21:09:10 -0400

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   allClients.push(socket);
   var UserID = allClients.indexOf(socket);
-  io.emit('userConnect', (UserID, allClients.length - 1));
+  io.emit('userConnect', {UserID: UserID, laUID: allClients.length});
   console.log('ID: ' + UserID + ' connected.');
 
   socket.on('disconnect', function(){
