327abe0f13939db7e6c3afaaea8ea6c57cf04392
UnknownEvil <unknownevil12@gmail.com>
2016-10-16 04:22:00 -0400

    +

--- scripts/main.js
+++ scripts/main.js
@@ -213,7 +213,7 @@ socket.on('spawnBird', function (data) {
 	game.physics.p2.enable(bird[bird.length - 1], true);
 	bird[bird.length - 1].body.angle = (Math.random() * 160) + 10
 	bird[bird.length - 1].body.setZeroRotation();
-	bird[bird.length - 1].sprite.body.thrust(Math.random() * 100);
+	bird[bird.length - 1].body.thrust(Math.random() * 100);
 	bird[bird.length - 1].body.kinematic = true;
 	bird[bird.length - 1].body.fixedRotation = true;
 	if (bird[bird.length - 1].body.angle > 90) bird[bird.length - 1].scale.setTo(1, -1);
