f779d22184aea7f86635594c14c3e501a97164f9
UnknownEvil <unknownevil12@gmail.com>
2016-10-16 04:49:48 -0400

    +

--- scripts/main.js
+++ scripts/main.js
@@ -208,7 +208,7 @@ socket.on('spawnBird', function (data) {
 	bird[bird.length] = game.add.sprite(data.x, data.y, 'bird');
 	bird[bird.length - 1].index = bird.length - 1;
 	game.physics.p2.enable(bird[bird.length - 1], true);
-	bird[bird.length - 1].body.angle = (Math.random() * 90) - 90;
+	bird[bird.length - 1].body.angle = (Math.random() * 180) - 90;
 	//bird[bird.length - 1].body.fixedRotation = true;
 	if (bird[bird.length - 1].body.angle > 90) bird[bird.length - 1].scale.setTo(-1, 1);
 	bird[bird.length - 1].body.collideWorldBounds = false;
