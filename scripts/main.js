543f67b2c639f8e1554600fad2a1deff129f30c5
UnknownEvil <unknownevil12@gmail.com>
2016-10-15 23:26:33 -0400

    +

--- scripts/main.js
+++ scripts/main.js
@@ -74,8 +74,8 @@ function update() {
 window.onmousemove = function () {
 	var mouseX = game.input.mousePointer.x;
 	var mouseY = game.input.mousePointer.y;
-	cursor[mUID].x = mouseX;
-	cursor[mUID].y = mouseY;
+	cursor[mUID].x = mouseX - 16;
+	cursor[mUID].y = mouseY - 16;
 	
 	
 	socket.emit('moveCursor', {
