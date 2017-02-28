9af8ae912934ae0c2050e6ede0ff086ebfdc534e
UnknownEvil <reapor12@hotmail.com>
2017-02-28 10:35:55 -0500

    Create index.php

---
+++ register/index.php
@@ -0,0 +1,8 @@
+<!doctype html>
+<body>
+  <form>
+    <input type="text" placeholder="username"></input>
+    <input type="text" placeholder="password"></input>
+    <input type="button"></input>
+  </form>
+</body>
