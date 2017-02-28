751f10f8e9f64d16c3c8ebd35698aaff2a1b40ed
UnknownEvil <reapor12@hotmail.com>
2017-02-28 10:40:45 -0500

    Update index.php

--- register/index.php
+++ register/index.php
@@ -1,8 +1,10 @@
 <!doctype html>
+<head>
+</head>
 <body>
   <form>
-    <input type="text" placeholder="username"></input>
-    <input type="text" placeholder="password"></input>
-    <input type="button"></input>
+    <input type="text" placeholder="username">
+    <input type="text" placeholder="password">
+    <input type="button" value="Register">
   </form>
 </body>
