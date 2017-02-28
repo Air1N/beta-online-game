62fc7314b8691947ed14b447255ea4c1188f7d47
UnknownEvil <reapor12@hotmail.com>
2017-02-28 10:42:36 -0500

    Update index.php

--- register/index.php
+++ register/index.php
@@ -3,8 +3,9 @@
 </head>
 <body>
   <form>
-    <input type="text" placeholder="username">
-    <input type="text" placeholder="password">
+    <input type="text" placeholder="Username">
+    <input type="text" placeholder="Email" disabled value="Temporarily Disabled">
+    <input type="password" placeholder="Password">
     <input type="button" value="Register">
   </form>
 </body>
