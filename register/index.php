d3810490c702762a7a1823187bc06f588bb9f12e
UnknownEvil <reapor12@hotmail.com>
2017-02-28 10:52:21 -0500

    Update index.php

--- register/index.php
+++ register/index.php
@@ -1,11 +1,19 @@
+<?php
+if (isset($_POST['register'])) {
+  $username = $_POST['username'];
+  $password = $_POST['password'];
+  
+  
+}
+?>
 <!doctype html>
 <head>
 </head>
 <body>
-  <form>
-    <input type="text" placeholder="Username"><br>
-    <input type="text" placeholder="Email" disabled><br>
-    <input type="password" placeholder="Password"><br>
-    <input type="button" value="Register">
+  <form method="post">
+    <input type="text" name="username" placeholder="Username"><br>
+    <input type="text" name="email" placeholder="Email" disabled><br>
+    <input type="password" name="password" placeholder="Password"><br>
+    <input type="submit" name="register" value="Register">
   </form>
 </body>
