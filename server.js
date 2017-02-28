24166c893030f02ea1ec017fa26a91e08b3e5491
UnknownEvil <reapor12@hotmail.com>
2017-02-28 10:12:11 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,57 +3,19 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 80;
-var connections = 0;
-var allClients = -1;
+var allClients = [];
+
 
 app.use('/', express.static(__dirname + '/'));
 
-/*app.get('/', function (req, res) {
-	res.sendFile(__dirname + '/index.html');
-});*/
-
 io.sockets.on('connection', function (socket) {
-	allClients++;
-	var UserID = allClients;
-	io.sockets.emit('userConnect', {
-		UserID : UserID,
-		laUID : allClients
-	});
-	console.log('ID: ' + UserID + ' connected.');
-
-	socket.on('disconnect', function () {
-		console.log('ID: ' + UserID + ' disconnected.');
-		io.sockets.emit('userDisconnect', UserID);
-		allClients--;
-	});
-
-	socket.on('chat message', function (msg) {
-		io.sockets.emit('chat message', msg);
-	});
-
-	socket.on('updatePos', function (data) {
-		io.sockets.emit('updatePos', data);
-	});
-
-	socket.on('move', function (data) {
-		io.sockets.emit('move', data);
-	});
-
-	socket.on('spawnBird', function (data) {
-		io.sockets.emit('spawnBird', data);
-	});
-
-	socket.on('moveCursor', function (data) {
-		io.sockets.emit('moveCursor', data);
-	});
-	
-	socket.on('birdKill', function (data) {
-		io.sockets.emit('birdKill', data);
-	});
-	
-	socket.on('newData', function (data) {
-		io.sockets.emit('newData', data);
-	});
+  allClients.push(socket);
+  var ID = allClients.indexOf(socket);
+  console.debug("ID " + ID + " connected.");
+  socket.on('disconnect', function() {
+    console.debug("ID " + ID + " disconnected.");
+    allClients.splice(ID, 1);
+  });
 });
 
 http.listen(port, function () {
