df03f256e789c9f5c0204f1b5b31a98c8bd85121
UnknownEvil <reapor12@hotmail.com>
2017-03-02 10:27:28 -0500

    Update index.php

--- register/index.php
+++ register/index.php
@@ -1,31 +1,8 @@
 <?php
 if (isset($_POST['register'])) {
   $username = $_POST['username'];
-  $password = $_POST['password'];
-  
-  
+  $password = $_POST['password']; 
 }
-
-$servername = "localhost";
-$username = "username";
-$password = "password";
-
-// Create connection
-$conn = new mysqli($servername, $username, $password);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-// Create database
-$sql = "CREATE DATABASE myDB";
-if ($conn->query($sql) === TRUE) {
-    echo "Database created successfully";
-} else {
-    echo "Error creating database: " . $conn->error;
-}
-
-$conn->close();
 ?>
 <!doctype html>
 <head>
