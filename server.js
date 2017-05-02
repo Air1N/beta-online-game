2a96e76766f30ce6fecd7531ea0b12e2cd17610a
UnknownEvil <reapor12@hotmail.com>
2017-05-02 10:49:18 -0400

    Add files via upload

--- server.js
+++ server.js
@@ -3,19 +3,59 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var port = process.env.PORT || 80;
-var allClients = [];
+var connections = 0;
+var allClients = -1;
 
+app.use('/assets', express.static(__dirname + '/assets'));
+app.use('/scripts', express.static(__dirname + '/scripts'));
+app.use('/lib', express.static(__dirname + '/lib'));
 
-app.use('/', express.static(__dirname + '/'));
+app.get('/', function (req, res) {
+	res.sendFile(__dirname + '/index.html');
+});
 
 io.sockets.on('connection', function (socket) {
-  allClients.push(socket);
-  var ID = allClients.indexOf(socket);
-  console.log("ID " + ID + " connected.");
-  socket.on('disconnect', function() {
-    console.log("ID " + ID + " disconnected.");
-    allClients.splice(ID, 1);
-  });
+	allClients++;
+	var UserID = allClients;
+	io.sockets.emit('userConnect', {
+		UserID : UserID,
+		laUID : allClients
+	});
+	console.log('ID: ' + UserID + ' connected.');
+
+	socket.once('disconnect', function () {
+		console.log('ID: ' + UserID + ' disconnected.');
+		io.sockets.emit('userDisconnect', UserID);
+		allClients--;
+	});
+
+	socket.on('chat message', function (msg) {
+		io.sockets.emit('chat message', msg);
+	});
+
+	socket.on('updatePos', function (data) {
+		io.sockets.emit('updatePos', data);
+	});
+
+	socket.on('move', function (data) {
+		io.sockets.emit('move', data);
+	});
+
+	socket.on('spawnBird', function (data) {
+		io.sockets.emit('spawnBird', data);
+	});
+
+	socket.on('moveCursor', function (data) {
+		io.sockets.emit('moveCursor', data);
+	});
+	
+	socket.on('birdKill', function (data) {
+		io.sockets.emit('birdKill', data);
+	});
+	
+	socket.on('newData', function (data) {
+		io.sockets.emit('newData', data);
+	});
 });
 
 http.listen(port, function () {
